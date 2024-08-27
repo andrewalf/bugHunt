08f74497ae374d79f7ee20287f6a6294f65e7eaf
Benzodrel <wizardofearth@yandex.ru>
2024-08-27 15:22:13 +0300

    fed-35 added chunk products reading to avoid error in ProductExportController

--- app/Http/Controllers/ProductExportController.php
+++ app/Http/Controllers/ProductExportController.php
@@ -7,29 +7,32 @@ use Illuminate\Support\Facades\Response;
 
 class ProductExportController extends Controller
 {
+    private const CHUNK_SIZE = 50;
     public function index()
     {
-        $products = Product::all();
+
         $file = fopen('php://memory', 'w');
         fputcsv($file, ['Название', 'Описание', 'Цена', 'Остаток', 'Видимость', 'Время_создания', 'URL_изображения']);
 
-        foreach ($products as $product) {
-            fputcsv($file, [
-                $product->name,
-                $product->description,
-                $product->price,
-                $product->stock,
-                $product->is_visible == 1?"Да":"Нет",
-                $product->created_at,
-                $product->getImageUrl()
-            ]);
-        }
+            $products = Product::chunk(self::CHUNK_SIZE, function ($products) use ($file) {
+                foreach ($products as $product) {
+                    fputcsv($file, [
+                        $product->name,
+                        $product->description,
+                        $product->price,
+                        $product->stock,
+                        $product->is_visible == 1 ? "Да" : "Нет",
+                        $product->created_at,
+                        $product->getImageUrl()
+                    ]);
+                }
+            });
 
         fseek($file, 0);
 
-        return Response::streamDownload(function() use ($file) {
+        return Response::streamDownload(function () use ($file) {
             fpassthru($file);
-        }, date("Y-m-d H:i:s") .'_products.csv', [
+        }, date("Y-m-d H:i:s") . '_products.csv', [
             'Content-Type' => 'text/csv',
             'Content-Disposition' => 'attachment; filename="products.csv"',
         ]);
